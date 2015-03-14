d60b48355d15f4f5ec378b4fca3a69c5187dd9db
Citringo <citringo@gmail.com>
2015-03-15 00:46:33 +0900

    0~10 -> 0~9

--- index.php
+++ index.php
@@ -147,7 +147,7 @@ if ($pagename == "") { //まっしろけならfrontpage
 					echo getmes();
 					function getmes()
 					{
-						$i = rand(0, 10);
+						$i = rand(0, 9);
 						switch($i)
 						{
 							case 0: return "ฅ(º ﾛ º ฅ)＜にょほーーーーwwwwwww";
